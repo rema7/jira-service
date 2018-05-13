26166d98eb3f3575926e7ba8d77707c35f149872
Maxim CHerkasov <maxim.cherkasov@gmail.com>
2018-05-13 23:25:19 +0300

    Add go-kit

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 	"github.com/andygrunwald/go-jira"
 	"github.com/BurntSushi/toml"
+	"context"
+	"errors"
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+	"net/http"
+	"encoding/json"
+	"log"
 )
 
 type tomlConfig struct {
@@ -34,6 +41,46 @@ func jiraConnect(url string, user string, password string) (*jira.Client, error)
 	return jira.NewClient(tp.Client(), url)
 }
 
+type JiraService interface {
+	GetIssue(context.Context, string) (string, error)
+}
+
+type jiraService struct{}
+
+func (jiraService) GetIssue(_ context.Context, issue string) (string, error) {
+	if issue == "" {
+		return "", ErrEmpty
+	}
+
+	return "XXXX---XXX", nil
+}
+
+func (jiraService) Count(_ context.Context, s string) int {
+	return len(s)
+}
+
+var ErrEmpty = errors.New("empty string")
+
+type issueRequest struct {
+	Issue string `json:"issue"`
+}
+
+type issueResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't define JSON marshaling
+}
+
+func getIssueEndpoint(js JiraService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(issueRequest)
+		v, err := js.GetIssue(ctx, req.Issue)
+		if err != nil {
+			return issueResponse{v, err.Error()}, nil
+		}
+		return issueResponse{v, ""}, nil
+	}
+}
+
 func main() {
 	config, err := readConfig()
 	if err != nil {
@@ -60,4 +107,28 @@ func main() {
 	fmt.Printf("Type: %s\n", issue.Fields.Type.Name)
 	fmt.Printf("Priority: %s\n", issue.Fields.Priority.Name)
 
+	js := jiraService{}
+
+	issueHandler := httptransport.NewServer(
+		getIssueEndpoint(js),
+		decodeIssueRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/uppercase", issueHandler)
+
+	log.Fatal(http.ListenAndServe(":9090", nil))
+
+}
+
+func decodeIssueRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request issueRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
 }
