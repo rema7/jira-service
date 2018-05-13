eec82e305d3122a756f2c6ffef6f88a85e3f105d
Maxim CHerkasov <maxim.cherkasov@gmail.com>
2018-05-13 22:47:17 +0300

    Jira connection and configuration

--- main.go
+++ main.go
@@ -7,27 +7,57 @@ import (
 )
 
 type tomlConfig struct {
-	Title   string
+	Jira jiraConfig
 }
 
-func readConfig() {
+type jiraConfig struct {
+	Url string
+	User  string
+	Password  string
+}
+
+func readConfig() (tomlConfig, error) {
 	var config tomlConfig
 	if _, err := toml.DecodeFile("settings.toml", &config); err != nil {
 		fmt.Println(err)
+		return tomlConfig{}, err
+	}
+	return config, nil
+}
+
+func jiraConnect(url string, user string, password string) (*jira.Client, error) {
+	tp := jira.CookieAuthTransport{
+		Username: user,
+		Password: password,
+		AuthURL:  fmt.Sprintf("%s/rest/auth/1/session", url),
+	}
+	return jira.NewClient(tp.Client(), url)
+}
+
+func main() {
+	config, err := readConfig()
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
-}
-func main() {
-	jiraClient, _ := jira.NewClient(nil, "https://issues.apache.org/jira/")
-	issue, _, _ := jiraClient.Issue.Get("MESOS-3325", nil)
+
+	jiraConfig := config.Jira
+	jiraClient, err := jiraConnect(jiraConfig.Url, jiraConfig.Password, jiraConfig.Password)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	issue, resp, err := jiraClient.Issue.Get("RM-1", nil)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println(resp)
+		return
+	}
 
 	fmt.Printf("%s: %+v\n", issue.Key, issue.Fields.Summary)
 	fmt.Printf("Type: %s\n", issue.Fields.Type.Name)
 	fmt.Printf("Priority: %s\n", issue.Fields.Priority.Name)
 
-	readConfig()
-
-	// MESOS-3325: Running mesos-slave@0.23 in a container causes slave to be lost after a restart
-	// Type: Bug
-	// Priority: Critical
 }
